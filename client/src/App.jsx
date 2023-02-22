e89430d28ac6c7317f3540e221f16f3f9bf2e1c1
takeru_tanaka <rhzxrzxk@gmail.com>
2023-02-22 21:50:21 +0900

    fix App.jsx for fundraiser

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,41 @@
-import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
+import React, { useState, useEffect } from "react";
+import FundraiserFactoryContract from "./contracts/FundraiserFactory.json";
+import getWeb3 from "./utils/getWeb3";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({ web3: null, accounts: null, contract: null });
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3 = await getWeb3();
+        const accounts = await web3.eth.getAccounts();
+        const netWorkId = await web3.eth.getId();
+        const deployedNetwork = FundraiserFactoryContract.networks[netWorkId];
+        const instance = new web3.eth.Contract(
+          FundraiserFactoryContract.abi,
+          deployedNetwork && deployedNetwork.address,
+        );
+        setState({web3, accounts, contract: instance});
+      } catch(error) {
+        alert(
+          'App.jsx: Failed to load web3, accounts, or contract. Check console for details.',
+        );
+        console.error(error);
+      }
+    }
+    init();
+  }, {});
+
+  const runExample = async () => {
+    const { accounts, contract } = state;
+  };
+
   return (
-    <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
-      </div>
-    </EthProvider>
+    <div>
+      <h1>Fundraiser</h1>
+    </div>
   );
 }
 
