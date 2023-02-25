25d1abc0088605fdbb339b9cdf25ff5a26d07474
takeru_tanaka <rhzxrzxk@gmail.com>
2023-02-25 18:21:09 +0900

    fix

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
 
-import {BrowserRouter, NavLink} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+
+import App from "./App";
+
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
